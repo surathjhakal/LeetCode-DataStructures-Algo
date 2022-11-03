51ac722d5426d3c5e4b3a217d9713da3aded8e9a
Surath Jhakal <46605283+surathjhakal@users.noreply.github.com>
2022-11-03 19:07:51 +0530

    Update next-permutation.java

--- Day1-Arrays/next-permutation.java
+++ Day1-Arrays/next-permutation.java
@@ -1,3 +1,16 @@
+  public static void reverse(int startPointer,int[] nums){
+      int pointer1=startPointer;
+      int pointer2=nums.length-1;
+      while(pointer1<pointer2){
+          swap(pointer1++,pointer2--,nums);
+      }
+  }
+
+  public static void swap(int i,int j, int[] nums){
+      int temp=nums[i];
+      nums[i]=nums[j];
+      nums[j]=temp;
+  }
   public static void nextPermutation(int[] nums){
       if(nums == null || nums.length <= 1) return;
       int i = nums.length - 2;
