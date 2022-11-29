9458aa9ede91f2559da949ac3303d889369d8cf3
Surath Jhakal <46605283+surathjhakal@users.noreply.github.com>
2022-11-29 19:29:39 +0530

    Create flattening-of-linkedlist.java

---
+++ Day6-LinkedList 2/flattening-of-linkedlist.java
@@ -0,0 +1,33 @@
+    public static Node mergeTwoLists(Node a, Node b) {
+        Node temp = new Node(0);
+        Node res = temp;
+        while(a != null && b != null) {
+            if(a.value < b.value) {
+                temp.bottom = a;
+                temp = temp.bottom;
+                a = a.bottom;
+            }
+            else {
+                temp.bottom = b;
+                temp = temp.bottom;
+                b = b.bottom;
+            }
+        }
+
+        if(a != null) temp.bottom = a;
+        else temp.bottom = b;
+        return res.bottom;
+
+    }
+    public static Node flatten(Node root)
+    {
+        if (root == null || root.next == null)
+            return root;
+        // recur for list on right
+        root.next = flatten(root.next);
+        // now merge
+        root = mergeTwoLists(root, root.next);
+        // return the root
+        // it will be in turn merged with its left
+        return root;
+    }
